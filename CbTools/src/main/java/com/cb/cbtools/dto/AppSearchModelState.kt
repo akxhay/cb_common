82a4a61d98fe60c4f9769c3e8fb035dd0e42675b
a13400273 <akshay.sharma@africa.airtel.com>
2023-05-13 22:45:13 +0530

    App info

---
+++ CbTools/src/main/java/com/cb/cbtools/dto/AppSearchModelState.kt
@@ -0,0 +1,15 @@
+package com.cb.cbtools.dto
+
+
+data class AppSearchModelState(
+    val searchText: String = "",
+    val apps: List<AppListInfo> = arrayListOf(),
+    val progressBar: Boolean = true,
+
+    ) {
+
+    companion object {
+        val Empty = AppSearchModelState()
+    }
+
+}
