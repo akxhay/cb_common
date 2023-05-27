2e8b6f5ae6e7ca5ed25009f5612ca221bb314290
a13400273 <akshay.sharma@africa.airtel.com>
2023-05-27 19:37:51 +0530

    openPlayStore method

--- CbTools/src/main/java/com/cb/cbtools/util/RateUtil.kt
+++ CbTools/src/main/java/com/cb/cbtools/util/RateUtil.kt
@@ -10,10 +10,17 @@ object RateUtil {
     fun rate(
         activity: Activity,
     ) {
+        openPlayStore(activity = activity, packageName = activity.applicationContext.packageName)
+    }
+
+    fun openPlayStore(
+        activity: Activity,
+        packageName: String
+    ) {
         try {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data =
-                Uri.parse("https://play.google.com/store/apps/details?id=" + activity.applicationContext.packageName)
+                Uri.parse("https://play.google.com/store/apps/details?id=" + packageName)
             activity.startActivity(intent)
 
         } catch (e: Exception) {
